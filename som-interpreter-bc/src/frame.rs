e73bbf43e6fa7239af092d31e9464032a5742f67
Octave Larose <octave.larose@hotmail.fr>
2024-07-04 14:01:17 +0100

    calling lookup_arg in Frame::nth_frame_back

--- som-interpreter-bc/src/frame.rs
+++ som-interpreter-bc/src/frame.rs
@@ -214,14 +214,15 @@ impl Frame {
             return current_frame;
         }
 
-        let mut target_frame: Rc<RefCell<Frame>> = match current_frame.borrow().args.first().unwrap() {
+        let mut target_frame: Rc<RefCell<Frame>> = match current_frame.borrow().lookup_argument(0) {
             Value::Block(block) => {
                 Rc::clone(block.frame.as_ref().unwrap())
             }
             v => panic!("attempting to access a non local var/arg from a method instead of a block: self wasn't blockself but {:?}.", v)
         };
+        
         for _ in 1..n {
-            target_frame = match Rc::clone(&target_frame).borrow().args.first().unwrap() {
+            target_frame = match Rc::clone(&target_frame).borrow().lookup_argument(0) {
                 Value::Block(block) => {
                     Rc::clone(block.frame.as_ref().unwrap())
                 }
