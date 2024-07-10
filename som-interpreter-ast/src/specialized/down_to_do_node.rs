d9b12f0469be2671ece0a7585c28428b4f7b7552
Octave Larose <octave.larose@hotmail.fr>
2024-07-10 10:33:35 +0100

    specializations in to:do: and downTo:do: nodes

--- som-interpreter-ast/src/specialized/down_to_do_node.rs
+++ som-interpreter-ast/src/specialized/down_to_do_node.rs
@@ -1,4 +1,5 @@
 use std::rc::Rc;
+use crate::block::Block;
 use crate::evaluate::Evaluate;
 use crate::invokable::{Invoke, Return};
 use crate::universe::UniverseAST;
@@ -13,11 +14,16 @@ impl Invoke for DownToDoNode {
         let end_int_val = args.get(1).unwrap();
         let body_block_val = args.get(2).unwrap();
 
-        let (start_int, end_int, body_block) = match (start_int_val, end_int_val, body_block_val) {
-            (Value::Integer(a), Value::Integer(b), Value::Block(c)) => (*a, *b, c.clone()),
+        match (start_int_val, end_int_val, body_block_val) {
+            (Value::Integer(a), Value::Integer(b), Value::Block(c)) => Self::do_int_loop(*a, *b, c.clone(), universe),
+            (Value::Double(a), Value::Double(b), Value::Block(c)) => Self::do_double_loop(*a, *b, c.clone(), universe),
             (a, b, c) => panic!("downTo:do: was not given two ints and a block as arguments, but {:?} and {:?} and {:?}", a, b, c)
-        };
+        }
+    }
+}
 
+impl DownToDoNode {
+    fn do_int_loop(start_int: i64, end_int: i64, body_block: Rc<Block>, universe: &mut UniverseAST) -> Return {
         let mut i = start_int;
         while i >= end_int {
             propagate!(universe.with_frame(
@@ -27,7 +33,19 @@ impl Invoke for DownToDoNode {
             ));
             i -= 1;
         }
-
         Return::Local(Value::Integer(start_int))
+    }
+
+    fn do_double_loop(start_double: f64, end_double: f64, body_block: Rc<Block>, universe: &mut UniverseAST) -> Return {
+        let mut i = start_double;
+        while i >= end_double {
+            propagate!(universe.with_frame(
+                body_block.block.nbr_locals,
+                vec![Value::Block(Rc::clone(&body_block)), Value::Double(i)],
+                |universe| body_block.evaluate(universe),
+            ));
+            i -= 1.0;
+        }
+        Return::Local(Value::Double(start_double))
     }
 }
