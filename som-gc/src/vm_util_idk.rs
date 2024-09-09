eae114a715685e266537199c7cd366991e0d2900
Octave Larose <octave.larose@hotmail.fr>
2024-09-09 12:30:27 +0100

    changing plan from NoGC to semispace

--- som-gc/src/vm_util_idk.rs
+++ som-gc/src/vm_util_idk.rs
@@ -7,12 +7,10 @@ pub fn init_gc() -> Box<Mutator<SOMVM>> {
     let mut builder = mmtk_create_builder();
     mmtk_init(&mut builder);
 
-    // Set option by value using extern "C" wrapper.
     let success = mmtk_set_fixed_heap_size(&mut builder, 1048576);
     assert!(success);
 
-    // not sure that's needed, but we *are* doing NoGC for now. CStrings DEFINITELY not needed.
-    let success = mmtk_set_option_from_string(&mut builder, "plan", "NoGC");
+    let success = mmtk_set_option_from_string(&mut builder, "plan", "SemiSpace");
     assert!(success);
 
     let tls = VMMutatorThread(VMThread(OpaquePointer::UNINITIALIZED)); // FIXME: Use the actual thread pointer or identifier
