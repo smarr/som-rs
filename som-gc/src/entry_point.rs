87b43ccfd834f2d2b455b87bba2594d7998aa20d
Octave Larose <octave.larose@hotmail.fr>
2024-09-12 16:12:43 +0100

    SemiSpace GC now seems operational! likely very unoptimized for now though

--- som-gc/src/entry_point.rs
+++ som-gc/src/entry_point.rs
@@ -5,17 +5,17 @@ use crate::SOMVM;
 
 pub fn init_gc() -> (VMMutatorThread, Box<Mutator<SOMVM>>) {
     let mut builder = mmtk_create_builder();
+
+    // let heap_success = mmtk_set_fixed_heap_size(&mut builder, 1048576);
+    // assert!(heap_success, "Couldn't set MMTk fixed heap size");
+
+    let gc_success = builder.set_option("plan", "SemiSpace");
+    assert!(gc_success, "Couldn't set GC plan");
+    
+    // builder.options.threads.set(1);
+
     mmtk_init(&mut builder);
-
-    let success = mmtk_set_fixed_heap_size(&mut builder, 1048576);
-    assert!(success, "Couldn't set MMTk fixed heap size");
-
-    let success = mmtk_set_option_from_string(&mut builder, "plan", "SemiSpace");
-    assert!(success, "Couldn't set GC plan");
-
-    // todo remove/change?
-    builder.options.threads.set(1);
-
+    
     // VMMutatorThread(VMThread::)
     let tls = VMMutatorThread(VMThread(OpaquePointer::UNINITIALIZED)); // FIXME: Use the actual thread pointer or identifier
     let mutator = mmtk_bind_mutator(tls);
