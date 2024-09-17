dd775b6e653f24e6df8179c48839299264d0a3b8
Octave Larose <octave.larose@hotmail.fr>
2024-09-17 10:12:09 +0100

    caching allocator and dispatching to that instead

--- som-core/src/gc.rs
+++ som-core/src/gc.rs
@@ -3,6 +3,8 @@ use mmtk::util::{Address, VMMutatorThread};
 use som_gc::SOMVM;
 use std::marker::PhantomData;
 use mmtk::{AllocationSemantics, Mutator};
+use mmtk::util::alloc::{Allocator, BumpAllocator};
+use mmtk::util::constants::MIN_OBJECT_SIZE;
 // use mmtk::util::alloc::BumpAllocator;
 use som_gc::api::{mmtk_alloc, mmtk_destroy_mutator, mmtk_handle_user_collection_request, mmtk_post_alloc};
 use som_gc::entry_point::init_gc;
@@ -12,9 +14,9 @@ static GC_ALIGN: usize = 8;
 static GC_SEMANTICS: AllocationSemantics = AllocationSemantics::Default;
 
 pub struct GCInterface {
-    mutator_thread: VMMutatorThread,
     mutator: Box<Mutator<SOMVM>>,
-    // pub default_allocator: Box<BumpAllocator<SOMVM>>
+    mutator_thread: VMMutatorThread,
+    default_allocator: *mut BumpAllocator<SOMVM>,
 }
 
 impl Drop for GCInterface {
@@ -26,11 +28,11 @@ impl Drop for GCInterface {
 impl GCInterface {
     /// Initialize the GCInterface. Internally inits MMTk and fetches everything needed to actually communicate with the GC. 
     pub fn init() -> Self {
-        let (mutator_thread, mutator) = init_gc();
+        let (mutator_thread, mutator, default_allocator) = init_gc();
         Self {
             mutator_thread,
             mutator,
-            // default_allocator
+            default_allocator
         }
     }
     
@@ -106,8 +108,8 @@ impl<T> GCRef<T> {
 impl<T> GCRef<T> {
     // Allocates a type on the heap and returns a pointer to it.
     pub fn alloc(obj: T, gc_interface: &mut GCInterface) -> GCRef<T> {
-        // Self::alloc_with_size_cached_allocator(obj, gc_interface, size_of::<T>())
-        Self::alloc_with_size(obj, gc_interface, size_of::<T>())
+        Self::alloc_with_size_cached_allocator(obj, gc_interface, size_of::<T>())
+        // Self::alloc_with_size(obj, gc_interface, size_of::<T>())
     }
 
     // Allocates a type, but with a given size. Useful when an object needs more than what we tell Rust through defining a struct. 
@@ -132,29 +134,29 @@ impl<T> GCRef<T> {
         }
     }
     
-    // pub fn alloc_with_size_cached_allocator(obj: T, gc_interface: &mut GCInterface, size: usize) -> GCRef<T> {
-    //     debug_assert!(size >= MIN_OBJECT_SIZE);
-    //     let addr = gc_interface.default_allocator.alloc(size, GC_ALIGN, GC_OFFSET);
-    //     debug_assert!(!addr.is_zero());
-    //
-    //     // println!("{}", mmtk_free_bytes());
-    //
-    //     // let obj = SOMVM::object_start_to_ref(addr);
-    //     // let space = allocator.get_space();
-    //     // debug_assert!(!obj.to_raw_address().is_zero());
-    //     // space.initialize_object_metadata(obj, true);
-    //
-    //     gc_interface.default_allocator.get_space().initialize_object_metadata(SOMVM::object_start_to_ref(addr), true);
-    //
-    //     unsafe {
-    //         *addr.as_mut_ref() = obj;
-    //     }
-    //
-    //     GCRef {
-    //         ptr: addr,
-    //         _phantom: PhantomData,
-    //     }
-    // }
+    pub fn alloc_with_size_cached_allocator(obj: T, gc_interface: &mut GCInterface, size: usize) -> GCRef<T> {
+        debug_assert!(size >= MIN_OBJECT_SIZE);
+        let allocator = unsafe {&mut (*gc_interface.default_allocator)};
+        let addr = allocator.alloc(size, GC_ALIGN, GC_OFFSET);
+        debug_assert!(!addr.is_zero());
+    
+    
+        // let obj = SOMVM::object_start_to_ref(addr);
+        // let space = allocator.get_space();
+        // debug_assert!(!obj.to_raw_address().is_zero());
+        // space.initialize_object_metadata(obj, true);
+    
+        allocator.get_space().initialize_object_metadata(SOMVM::object_start_to_ref(addr), true);
+    
+        unsafe {
+            *addr.as_mut_ref() = obj;
+        }
+    
+        GCRef {
+            ptr: addr,
+            _phantom: PhantomData,
+        }
+    }
 }
 
 /// Custom alloc function.
--- som-gc/src/entry_point.rs
+++ som-gc/src/entry_point.rs
@@ -1,10 +1,12 @@
-use mmtk::{Mutator};
+use mmtk::{memory_manager, AllocationSemantics, Mutator};
+use mmtk::util::Address;
+use mmtk::util::alloc::BumpAllocator;
 use crate::api::*;
 use mmtk::util::opaque_pointer::*;
 use crate::{SINGLETON, SOMVM};
 
-// pub fn init_gc() -> (VMMutatorThread, Box<Mutator<SOMVM>>, Box<BumpAllocator<SOMVM>>) {
-pub fn init_gc() -> (VMMutatorThread, Box<Mutator<SOMVM>>) {
+pub fn init_gc() -> (VMMutatorThread, Box<Mutator<SOMVM>>, *mut BumpAllocator<SOMVM>) {
+// pub fn init_gc() -> (VMMutatorThread, Box<Mutator<SOMVM>>) {
     if SINGLETON.get().is_none() {
         let mut builder = mmtk_create_builder();
 
@@ -27,22 +29,22 @@ pub fn init_gc() -> (VMMutatorThread, Box<Mutator<SOMVM>>) {
     let tls = VMMutatorThread(VMThread(OpaquePointer::UNINITIALIZED)); // TODO: do I need a thread pointer here?
     let mutator = mmtk_bind_mutator(tls);
 
-/*    let selector = memory_manager::get_allocator_mapping(
+    let selector = memory_manager::get_allocator_mapping(
         SINGLETON.get().unwrap(),
         AllocationSemantics::Default,
     );
     let default_allocator_offset = Mutator::<SOMVM>::get_allocator_base_offset(selector);
 
     // At run time: allocate with the default semantics without resolving allocator
-    let default_allocator: Box<BumpAllocator<SOMVM>> = {
+    let default_allocator: *mut BumpAllocator<SOMVM> = {
         let mutator_addr = Address::from_ref(&*mutator);
         unsafe {
             let ptr = mutator_addr + default_allocator_offset;
-            Box::from_raw(ptr.as_mut_ref())
+            ptr.as_mut_ref()
             // (mutator_addr + default_allocator_offset).as_mut_ref::<BumpAllocator<SOMVM>>()
         }
-    };*/
+    };
     
-    (tls, mutator)
-    // (tls, mutator, default_allocator)
+    // (tls, mutator)
+    (tls, mutator, default_allocator)
 }
