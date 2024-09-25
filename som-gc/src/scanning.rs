9b106de3092247cd07bd1775d2434b2bcb845549
Octave Larose <octave.larose@hotmail.fr>
2024-09-25 16:50:58 +0100

    fixed heap size for GC

--- som-gc/src/scanning.rs
+++ som-gc/src/scanning.rs
@@ -11,6 +11,21 @@ pub struct VMScanning {}
 
 // Documentation: https://docs.mmtk.io/api/mmtk/vm/scanning/trait.Scanning.html
 impl Scanning<SOMVM> for VMScanning {
+    fn scan_object<SV: SlotVisitor<SOMSlot>>(
+        _tls: VMWorkerThread,
+        _object: ObjectReference,
+        _slot_visitor: &mut SV,
+    ) {
+        todo!()
+    }
+
+    fn scan_object_and_trace_edges<OT: ObjectTracer>(_tls: VMWorkerThread, _object: ObjectReference, _object_tracer: &mut OT) {
+        todo!()
+    }
+
+    fn notify_initial_thread_scan_complete(_partial_scan: bool, _tls: VMWorkerThread) {
+        unimplemented!()
+    }
     fn scan_roots_in_mutator_thread(
         _tls: VMWorkerThread,
         _mutator: &'static mut Mutator<SOMVM>,
@@ -19,19 +34,6 @@ impl Scanning<SOMVM> for VMScanning {
         unimplemented!()
     }
     fn scan_vm_specific_roots(_tls: VMWorkerThread, _factory: impl RootsWorkFactory<SOMSlot>) {
-        unimplemented!()
-    }
-    fn scan_object<SV: SlotVisitor<SOMSlot>>(
-        _tls: VMWorkerThread,
-        _object: ObjectReference,
-        _slot_visitor: &mut SV,
-    ) {
-        todo!()
-    }
-    fn scan_object_and_trace_edges<OT: ObjectTracer>(_tls: VMWorkerThread, _object: ObjectReference, _object_tracer: &mut OT) {
-        todo!()
-    }
-    fn notify_initial_thread_scan_complete(_partial_scan: bool, _tls: VMWorkerThread) {
         unimplemented!()
     }
     fn supports_return_barrier() -> bool {
